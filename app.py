ab8a5c21561a6d1f0e7b671c832db41bbb8f367e
storymachinedata <120018282+storymachinedata@users.noreply.github.com>
2023-03-14 17:53:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,46 @@
-import dash
-#import dash_core_components as dcc 
-from dash import dcc
-#import dash_html_components as html
-from dash import html
-from dash.dependencies import Input, Output, State
-from pull_news2 import get_news
-from dash.exceptions import PreventUpdate
-import os
+# here we download pycountry pacakge to convert the country codes in two alphabet form which are
+# basically the country code
 
-#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-server = app.server
+import streamlit as st
+import requests as re
+import pycountry as py
+# from api import apiKEY
+
+myKey= 'ebaa63cf9dfa430d8f20bb9f7726ed03'
+st.title('News App for Storymachine Based on Country')
+# now for dividing whole screen into two parts we can use st.columns
+col1,col2=st.columns([3,1]) # 75,25
+
+with col1:
+    user=st.text_input('Enter country name')
+    btn= st.button('Enter')
 
 
-app = dash.Dash()
+if btn:
+    country=py.countries.get(name=user).alpha_2
+    url=f"https://newsapi.org/v2/top-headlines?country={country}&apiKey={myKey}"
+    r = re.get(url)
+    r=r.json()# here we have everything all the components of news
+    articles = r['articles']
 
-app.layout = html.Div([
-    dcc.Input(
-        id="query_string",
-        placeholder="Choose a country",
-        type="text"
-    ),
-    html.Button("Search", id="search_button"),
-    html.Div(id="content")
-])
+    print(articles[0])
 
-@app.callback(
-    Output("content", "children"),
-    [Input("search_button", "n_clicks")],
-    [State("query_string", "value")]
-)
+    for article in articles:
+        st.header(article['title'])
+        if article['author']:
+            st.write('Author:',article['author'])
+        st.write('Source:',article['source']['name'])
+        st.write('Url:',article['url'])
+        st.write('Published At:',article['publishedAt'])
 
-def get_news_callback(v, q):
-    if v!= None:
-        news = get_news(q)
-    else:
-        raise PreventUpdate
 
-    html_string = []
+        try:
+            st.image(article['urlToImage'])
+        except Exception:
+            pass
 
-    for n in news:
-        html_string.append(
-            html.Div([
-                html.H1([n[0]], className="title"),
-                html.P(n[1], className="p" ),
-                html.P(n[2]),
-                html.P(n[3])
-            ])
-        )
 
-    return html_string
-    
 
-#app.run_server(debug=True)
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+
+
