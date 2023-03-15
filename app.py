ac4d7adbd7bd55d9adf325e9822bd1673da8b554
storymachinedata <120018282+storymachinedata@users.noreply.github.com>
2023-03-15 13:26:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,18 @@ import pycountry as py
 # from api import apiKEY
 
 myKey= 'ebaa63cf9dfa430d8f20bb9f7726ed03'
-st.title('News App for Storymachine Based on Country')
+st.title('News App')
 # now for dividing whole screen into two parts we can use st.columns
-col1,col2=st.columns([3,1]) # 75,25
+col1,col2=st.columns([2,1]) # 75,25
 
 with col1:
-    user=st.text_input('Enter country name')
+    #user=st.text_input('Enter country name')
+    user = st.selectbox('Choose your country',
+            ('Argentina','Australia','Austria','Belgium','Brazil','Canada','China','Czechia','France','Germany','Greece','Hungary','India','Ireland','Israel','Italy','Japan','Mexico','Netherlands','Poland','Portugal','Russia','Sweden','Turkey','Ukraine','United Kingdom','United States'))
     btn= st.button('Enter')
+
+with col2:
+   st.image("https://www.storymachine.de/assets2/img/storymachine.png", width=200)
 
 
 if btn:
@@ -23,13 +28,13 @@ if btn:
     r=r.json()# here we have everything all the components of news
     articles = r['articles']
 
-    print(articles[0])
+    #print(articles[0])
 
     for article in articles:
         st.header(article['title'])
         if article['author']:
             st.write('Author:',article['author'])
-        st.write('Source:',article['source']['name'])
+        #st.write('Source:',article['source']['name'])
         st.write('Url:',article['url'])
         st.write('Published At:',article['publishedAt'])
 
